843a5a1375a317d6e329b9b22c82931062dba34d
DNX C <dnx@deniapps.com>
2022-10-19 21:48:34 -0400

    update deploy branch

--- deployad.sh
+++ deployad.sh
@@ -31,6 +31,6 @@ git commit -m 'deploy'
 # AGAIN! 把本文件另存为local_deploy.sh，然后修改下面的部署地址
 # gh_access_token 必须再你的环境变量里。如果你用的是mac，可以加到~/.zschrc里
 # 如何设置github access token: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
-git push -f "https://${gh_access_token}@github.com/deniapps/vns.git" main:vns-demo
+git push -f "https://${gh_access_token}@github.com/deniapps/vns.git" main:vns-ad
 
 cd -
